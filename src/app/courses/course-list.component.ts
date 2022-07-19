e0b71d0b81728d34bf9e9c57081a6af358dd500b
Matheus Fellipi <matheus.fellipi@outlook.com>
2022-07-19 02:05:28 -0300

    Criando a classe curso.

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,10 +1,31 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
+import { Course } from './course';
 
 @Component({
-  selector:'app-course-list',
-  templateUrl: './course-list.component.html'
+  selector: 'app-course-list',
+  templateUrl: './course-list.component.html',
 })
+export class CourserListComponent implements OnInit {
+  coursers: Course[] = [];
 
-export class CourserListComponent{
-  
+  ngOnInit(): void {
+    this.coursers = [
+      {
+        id: 1,
+        name: 'Angular: forms',
+        price: 99.99,
+        code: 'XPS-7856',
+        duration: 120,
+        rating: 5,
+      },  {
+        id: 2,
+        name: 'Angular: HTTP',
+        price: 40.99,
+        code: 'LKU-4531',
+        duration: 80,
+        rating: 4,
+      },
+      
+    ];
+  }
 }
---
+++ src/app/courses/course.ts
@@ -0,0 +1,8 @@
+export class Course {
+  id: number | undefined;
+  name: string | undefined;
+  price: number | undefined;
+  code: string | undefined;
+  duration: number | undefined;
+  rating: number | undefined;
+}
