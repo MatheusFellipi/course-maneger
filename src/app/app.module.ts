b84d0a4fe190e2678db03de49e8cf10ed4ae8bd3
Matheus Fellipi <matheus.fellipi@outlook.com>
2022-07-19 16:15:53 -0300

    Adicionando rotas o componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,44 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CourserListComponent } from './courses/course-list.component';
 import { StarComponent } from './courses/star/star.component';
+import { Error404 } from './error404/Error-404.component';
+import { NavBarComponent } from './navbar/navbar.component';
+import { ReplacePipe } from './piper/replace,pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourserListComponent,
-    StarComponent
+    StarComponent,
+    ReplacePipe,
+    NavBarComponent,
+    Error404,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: 'course',
+        pathMatch: 'full',
+      },
+      {
+        path: 'course',
+        component: CourserListComponent,
+      },
+      {
+        path: '**',
+        component: Error404,
+      },
+    ]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
