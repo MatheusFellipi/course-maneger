c07cbee6f18d0eeb5ffcd1ba4c74c1fb964ac4bf
Matheus Fellipi <matheus.fellipi@outlook.com>
2022-07-19 02:14:51 -0300

    Exibindo os dados.

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -13,17 +13,21 @@ export class CourserListComponent implements OnInit {
       {
         id: 1,
         name: 'Angular: forms',
+        Image:"",
         price: 99.99,
         code: 'XPS-7856',
         duration: 120,
         rating: 5,
+        release:"12/02/2022"
       },  {
         id: 2,
         name: 'Angular: HTTP',
+        Image:"",
         price: 40.99,
         code: 'LKU-4531',
         duration: 80,
         rating: 4,
+        release:"12/12/2020"
       },
       
     ];
--- src/app/courses/course.ts
+++ src/app/courses/course.ts
@@ -1,8 +1,10 @@
 export class Course {
+  Image:string|undefined;
   id: number | undefined;
   name: string | undefined;
   price: number | undefined;
   code: string | undefined;
   duration: number | undefined;
   rating: number | undefined;
+  release: string | undefined
 }
